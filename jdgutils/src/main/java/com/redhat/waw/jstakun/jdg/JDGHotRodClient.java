64356e4f761f47a3bc138784f48910f08d0d9603
jstakun <jstakun@redhat.com>
2016-05-29 10:34:30 +0200

    added cache arg

--- jdgutils/src/main/java/com/redhat/waw/jstakun/jdg/JDGHotRodClient.java
+++ jdgutils/src/main/java/com/redhat/waw/jstakun/jdg/JDGHotRodClient.java
@@ -9,17 +9,22 @@ public class JDGHotRodClient {
 	//java -cp "jdgutils-0.0.1-SNAPSHOT.jar:infinispan-client-hotrod-6.3.0.Final-redhat-5.jar:infinispan-commons-6.3.0.Final-redhat-5.jar:jboss-logging-3.1.4.GA-redhat-2.jar:commons-pool-1.6.jar:jboss-marshalling-1.4.10.Final-redhat-1.jar:jboss-marshalling-river-1.4.10.Final-redhat-1.jar" com.redhat.waw.jstakun.jdg.JDGHotRodClient
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		RemoteCache<String, String> sensors = getRemoteCache();
+		String cache = "default";
+		if (args.length > 0) {
+			cache = args[0];
+		} 
+		System.out.println("Cache " + cache + " will be used.");
+		
+		RemoteCache<String, String> sensors = getRemoteCache(cache);
 		sensors.put("sensordata", "test");
 		
-		System.out.println("Received sensor data: " + sensors.get("sensordata"));
+		System.out.println("Received cache data: " + sensors.get("sensordata"));
 
 	}
 
 	
-	public static RemoteCache<String, String> getRemoteCache() {
+	public static RemoteCache<String, String> getRemoteCache(String cache) {
 		ConfigurationBuilder builder = new ConfigurationBuilder();
 		builder.addServer()
 			.host("172.30.51.53").port(11333);
@@ -29,6 +34,6 @@ public class JDGHotRodClient {
 	        //    .serverName("tasks")
 	        //    .saslMechanism("DIGEST-MD5")
 	        //    .callbackHandler(new LoginHandler("thomas", "thomas-123".toCharArray(), "ApplicationRealm"));
-		return new RemoteCacheManager(builder.build(), true).getCache("sensors");
+		return new RemoteCacheManager(builder.build(), true).getCache(cache);
 	}
 }
