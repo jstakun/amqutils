bc60f57bc62b4571b2be6f6bd0b2068d92eae37a
jstakun <jstakun@redhat.com>
2016-06-02 11:50:42 +0200

    jdg client initial commit

---
+++ jdgclient/src/main/java/com/redhat/waw/jstakun/jdgclient/JDGService.java
@@ -0,0 +1,49 @@
+package com.redhat.waw.jstakun.jdgclient;
+
+import java.net.InetAddress;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import org.infinispan.client.hotrod.RemoteCache;
+import org.infinispan.client.hotrod.RemoteCacheManager;
+import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
+
+public class JDGService {
+
+	private static final String VERSION = "1.0.0";
+	
+	@GET
+	@Path("/info")
+	@Produces({"application/xml"})
+	public Response info() {
+		String addr = "127.0.0.1";
+		try {
+			InetAddress ip = InetAddress.getLocalHost();
+			addr = ip.getHostAddress();	  
+		} catch (Exception e) {
+			
+		}
+		return Response.status(200).entity("<info><name>JDG client service</name><version>" + VERSION + "</version><ip>" + addr + "</ip></info>").build();
+	}
+	
+	@GET
+	@Produces({"application/xml"})
+	public Response root() {
+	    return info();
+	}
+
+	public static RemoteCache<String, String> getRemoteCache(String cache, String host, int port) {
+		ConfigurationBuilder builder = new ConfigurationBuilder();
+		builder.addServer()
+			.host(host).port(port);
+			//.security()
+	        //.authentication()
+	        //    .enable()
+	        //    .serverName("tasks")
+	        //    .saslMechanism("DIGEST-MD5")
+	        //    .callbackHandler(new LoginHandler("admin", "manager1".toCharArray(), "ApplicationRealm"));
+		return new RemoteCacheManager(builder.build(), true).getCache(cache);
+	}
+}
