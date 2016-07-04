36c8c6fbb67c9bc460ab20a379f65f04e9139a7c
jstakun <jstakun@redhat.com>
2016-07-04 12:08:01 +0200

    modified endpoint

--- jdgclient/src/main/java/com/redhat/waw/jstakun/jdgclient/JDGService.java
+++ jdgclient/src/main/java/com/redhat/waw/jstakun/jdgclient/JDGService.java
@@ -24,7 +24,7 @@ import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
 import com.redhat.waw.iot.model.SensorData;
 import com.redhat.waw.ose.model.Decision;
 
-@Path("/sensor")
+@Path("/")
 public class JDGService {
 
 	private static final String VERSION = "1.0.0";
